65c083ca5b8e8235eb5f545466c334a043ad4f0f
Michael Cho <mcho314@gatech.edu>
2025-12-19 11:40:33 -0500

    Maint.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import yaml from '@rollup/plugin-yaml'; // Imported here
 
 // Import the V4 Vite plugin
 import tailwindcss from '@tailwindcss/vite';
@@ -9,11 +10,14 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://michael-j-cho.github.io',
 
-  // Standard Astro integrations (Notice: NO tailwind here)
+  // Standard Astro integrations
   integrations: [mdx(), sitemap()],
 
   // The new v4 way: Use Vite plugins
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      yaml() // <--- You must add this here to make it work!
+    ],
   },
 });
